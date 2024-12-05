c8689f958a57acec07deae628c7469790b87dde4
Kunambi <121929377+Wisdom-Kunambi@users.noreply.github.com>
2024-12-05 16:57:53 +0300

    add changes to the web file

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "<h1>Home Page</h1><a href='/about'>Go About</a>";
+});
+
+Route::get('/about', function() {
+    return "<h1>About Page</h1><a href='/'>Back to Home</a>";
 });
