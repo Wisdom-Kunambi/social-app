7dd31dacd4d57d4b6a6d5b57884d731bd7122567
Kunambi <121929377+Wisdom-Kunambi@users.noreply.github.com>
2024-12-06 18:54:16 +0300

    add changes to the ExampleController file and add new file and delete some file

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -8,10 +8,11 @@ use Illuminate\Http\Request;
 class ExampleController extends Controller
 {
     public function homePage(){
-
+        $animals = ['cat','dog','fox','panda','lion','tiger','elephant'];
+        return view('homepage', ['allAnimals' => $animals]);
     }
 
     public function aboutPage(){
-
+        return view('single-post');
     }
 }
